a885fb82eb48912acc1ec0beb37d7b8ae208f86d
Anthony <anthonyfeliperamirezfonseca@gmail.com>
2023-10-21 22:40:06 -0600

    Examen 1 de programacion 2

---
+++ Examen1/clsEmpleado.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Examen1
+{
+    internal class clsEmpleado
+    {
+        // atributos
+        static int cantidad = 2;
+        static public string[] cedula = new string[cantidad];
+        static public string[] nombre = new string[cantidad];
+        static public string[] direccion = new string[cantidad];
+        static public string[] telefono = new string[cantidad];
+        static public string[] salario = new string[cantidad];
+    
+
+
+        // metodos
+
+        public static string pidecedula()
+        {
+            Console.WriteLine("Ingrese la cedula");
+            string ced = Console.ReadLine();
+            return ced;
+        }
+        static public void Agregar()
+        {
+            Console.Clear();
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Digite el numero de cedula: {0}" ,i);
+                cedula[i] = Console.ReadLine();
+                Console.WriteLine("Digite el nombre: {0}",i);
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine("Ingrese la direccion: {0}" ,i);
+                direccion[i] = Console.ReadLine();
+                Console.WriteLine("Digite el numero de telefono: {0}" ,i);
+                telefono[i] = Console.ReadLine();
+                Console.WriteLine("Digite el salario: {0}" ,i);
+                salario[i] = Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+
+        static public void Consultar(string cedu)
+        {
+            bool encontrado = false;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (cedu.Equals(cedula[i]))
+                {
+                    Console.WriteLine($"Nombre:{nombre[i]} , la cedula: {cedula[i]} , la direccion: {direccion[i]}" +
+                        $" , el numero de telefono: {telefono[i]} y el salario: {salario[i]}");
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado == false)
+            {
+                Console.WriteLine("El empleado no ha sido encontrado");
+            }
+
+        }
+
+        static public void Modificar(string cedu)
+        {
+            bool encontrado = false;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (cedu.Equals(cedula[i]))
+                {
+                    Console.WriteLine("Digite el nombre: ");
+                    nombre[i] = Console.ReadLine() ;
+                    Console.WriteLine("Digite la cedula");
+                    cedula[i] = Console.ReadLine() ;
+                    Console.WriteLine("Digite la direccion: ");
+                    direccion[i] = Console.ReadLine() ;
+                    Console.WriteLine("Ingrese el numero de telefono: ");
+                    telefono[i] = Console.ReadLine() ;
+                    Console.WriteLine("Digite el salario:");
+                    salario[i] = Console.ReadLine();
+                  
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado == false)
+            {
+                Console.WriteLine("El empleado no ha sido encontrado");
+            }
+
+        }
+
+        static public void Borrar()
+        {
+            Console.WriteLine("Digite cedula: ");
+            string cedu = Console.ReadLine();
+            bool borrado = false;
+            for (int i = 0;i < cantidad; i++)
+            {
+                if (cedu.Equals(cedula[i]))
+                {
+                    Console.WriteLine("Digite el nombre: ");
+                    nombre[i] = Console.ReadLine();
+                    Console.WriteLine("Digite la direccion: ");
+                    direccion[0] = Console.ReadLine();
+                    Console.WriteLine("Ingrese el numero de telefono:");
+                    telefono[i] = Console.ReadLine();
+                    Console.WriteLine("Digite el salario: ");
+                    salario[i] = Console.ReadLine();
+                  // int.TryParse(Console.ReadLine(), out cantidad[i]);
+                    borrado = true;
+                    break;
+                }
+            }
+        }
+
+        static public void Inicializar()
+        {
+            cedula = Enumerable.Repeat("", cantidad).ToArray();
+            nombre = Enumerable.Repeat("", cantidad).ToArray();
+            direccion = Enumerable.Repeat("", cantidad).ToArray();
+            telefono = Enumerable.Repeat("", cantidad).ToArray();
+            salario = Enumerable.Repeat("", cantidad).ToArray();
+            Console.WriteLine("El sistema ha sido inicializado");
+        }
+        
+        static public void ListaEmpleado()
+        {
+            for (int i = 0; i < cedula.Length; i++)
+            {
+                    Console.WriteLine("Cedula");
+                    cedula[i] = Console.ReadLine();
+
+                    Console.WriteLine("Nombre: " + nombre[i] +  "Cedula: " + cedula[i] +  "Direccion: " + direccion[i] +
+                        "Numero de telefono: " + telefono[i] + "Salario: " + salario[i]);
+                
+            }
+        }
+
+        static public void Calculaprom()
+        {
+            List<double> salario = new List<double>();
+            double promedioSalarios = salario.Average();
+
+            Console.WriteLine("El promedio de salarios es de: {0}", salario.Average() );
+
+        }
+
+        static public void CalculaSalariomasAlto()
+        {
+           
+        }
+    }
+}
---
+++ Examen1/clsMenu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1
+{
+    internal class clsMenu
+    {
+        static int opcion = 0;
+
+        public static void Desplegar()
+        {
+            do
+            {
+                Console.WriteLine("1- Agregar Empleados");
+                Console.WriteLine("2- Consultar Empleados");
+                Console.WriteLine("3- Modificar Empleados");
+                Console.WriteLine("4- Borrar Empleado");
+                Console.WriteLine("5- Inicializar");
+                Console.WriteLine("6- Reportes");
+                Console.WriteLine("7- Salir");
+                int.TryParse(Console.ReadLine(), out opcion);
+
+                switch (opcion)
+                {
+                    case 1: clsEmpleado.Agregar(); break;
+                    case 2: clsEmpleado.Consultar(clsEmpleado.pidecedula()); break;
+                    case 3: clsEmpleado.Modificar(clsEmpleado.pidecedula()); break;
+                    case 4: clsEmpleado.Borrar(); break;
+                    case 5:
+                        clsEmpleado.Inicializar(); break;
+                    case 6: Console.Clear(); Submenu(); break;
+                    case 7: break;
+                    default:
+                        break;
+                }
+            } while (opcion != 7);
+        }
+        public static void Submenu()
+        {
+            do
+            {
+                Console.WriteLine("1- Consultar Empleados");
+                Console.WriteLine("2- Listar todos los empleados");
+                Console.WriteLine("3- Calcular y mostrar el promedio de los salarios");
+                Console.WriteLine("4- Calcular y mostrar el salario mas alto y bajo de todos los empleados");
+                Console.WriteLine("5- Salir");
+                int.TryParse(Console.ReadLine().Trim(), out opcion);
+                switch (opcion)
+                {
+                    case 1: 
+                        Console.WriteLine("Consulta Empleados");
+
+                        clsEmpleado.Consultar(clsEmpleado.pidecedula());
+
+                        break;
+                    case 2: Console.WriteLine("Lista Empleados");
+
+                        clsEmpleado.ListaEmpleado();
+                        break;
+                         
+                    case 3: Console.WriteLine("Calcula el promedio de los salarios"); 
+
+                        clsEmpleado.Calculaprom();
+                        break;   
+
+                    case 4: Console.WriteLine("Calcula salario mas alto");
+
+                        clsEmpleado.CalculaSalariomasAlto();
+                        break; 
+                    default:
+                        break;
+
+                }
+             } while (opcion != 5) ;
+        }
+     }
+ }
