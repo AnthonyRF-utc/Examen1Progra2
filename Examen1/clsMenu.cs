609c71418fecb341c4c56385914d47f67d05a93f
Anthony <anthonyfeliperamirezfonseca@gmail.com>
2023-10-21 22:42:56 -0600

    Agregando nombre

--- Examen1/clsMenu.cs
+++ Examen1/clsMenu.cs
@@ -5,6 +5,8 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
+// Examen Anthony Ramirez
+
 namespace Examen1
 {
     internal class clsMenu
